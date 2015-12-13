cadb012a11e117d4670aee80385a141bcf0c357c
Begin5257 <begin5257@gmail.com>
2015-12-13 13:54:27 +0800

    sqltest

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-$dsn ='SAE_MYSQL_HOST_M:SAE_MYSQL_PORT';
+$dsn = 'mysql:host=localhost;dbname=app_begin5257';
 $username = 'SAE_MYSQL_USER';
 $password = 'SAE_MYSQL_PASS';
+//$dsn ='SAE_MYSQL_HOST_M:SAE_MYSQL_PORT';
 //$dbname = SAE_MYSQL_DB;
 //$dsn = 'mysql:dbname = SAE_MYSQL_HOST_M';
 //$username ='SAE_MYSQL_USER';
