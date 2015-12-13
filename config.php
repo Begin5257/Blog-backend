5273b275340d711128129dd3867e8bd17dab0eeb
Begin5257 <begin5257@gmail.com>
2015-12-13 14:06:15 +0800

    修改数据库配置

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-$dsn = 'mysql:host=SAE_MYSQL_HOST_M;dbname=app_begin5257';
-$username = 'SAE_MYSQL_USER';
-$password = 'SAE_MYSQL_PASS';
+$dsn = 'mysql:host='.SAE_MYSQL_HOST_M.';dbname=app_begin5257';
+$username = SAE_MYSQL_USER;
+$password = SAE_MYSQL_PASS;
+echo $dsn;
 //$dsn ='SAE_MYSQL_HOST_M:SAE_MYSQL_PORT';
 //$dbname = SAE_MYSQL_DB;
 //$dsn = 'mysql:dbname = SAE_MYSQL_HOST_M';
