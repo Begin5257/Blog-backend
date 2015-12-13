58f8a1596ec367bc71d63ce5824da3e25a7212e0
Begin5257 <begin5257@gmail.com>
2015-12-13 22:00:40 +0800

    g

--- article-content.php
+++ article-content.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
     <title></title>
-    <link rel="stylesheet" type="text/css" href="css/article-content.css" media="screen/">
+    <link rel="stylesheet" type="text/css" href="css/article-content.css" media="screen"/>
 </head>
 <body>
 <div id="canvas-background">
