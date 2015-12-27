29eb1d222b503cd77565fb87efa97199e23f409b
Begin5257 <begin5257@gmail.com>
2015-12-27 21:08:51 +0800

    favicon

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-    <title></title>
+    <title>Beginning</title>
     <link rel="stylesheet" type="text/css" href="css/fonts.css" media="screen"/>
     <!--[if IE]>
     <script src="http://libs.useso.com/js/html5shiv/3.7/html5shiv.min.js"></script>
