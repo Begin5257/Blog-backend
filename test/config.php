21fab115337da26a1ca38093a4ee26e21ddb755b
Begin5257 <begin5257@gmail.com>
2016-01-03 22:16:46 +0800

    整理

--- test/config.php
+++
@@ -1,8 +0,0 @@
-<form method="post" action="insert.php">
-
-    <input type="text" id="username" name="username"  /><br/>
-
-    <input type="text" id="info" name="info"/><br/>
-
-    <input type="submit" value="Submit" name="submit"/>
-</form>
