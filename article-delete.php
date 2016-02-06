e4e76b0d3ad2840f41ae6318fefd37d9213c3c2b
Begin5257 <begin5257@gmail.com>
2016-02-06 22:17:08 +0800

    include header

--- article-delete.php
+++ article-delete.php
@@ -5,40 +5,9 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
     <title></title>
     <link rel="stylesheet" type="text/css" href="css/article-list.css" media="screen"/>
-    <!--[if IE]>
-    <script src="http://libs.useso.com/js/html5shiv/3.7/html5shiv.min.js"></script>
-    <![endif]-->
 </head>
 <body>
-<div id="nav-background">
-    <div id="canvas" class="gradient"></div>
-    <div id="transparent"></div>
-    <div class="nav fix">
-        <ul class="nav-ul">
-            <li class="nav-left">
-                <a href="#">
-                    <img src="icon/QQ图片20151123171438.jpg">
-                </a>
-            </li>
-            <li class="nav-li"><a href="#footer">关于我</a> </li>
-            <li class="nav-li"><a href="article-category.php">分类</a> </li>
-            <li class="nav-li"><a href="article-list.php">文章</a> </li>
-            <li class="nav-li"><a href="index.php">首页</a> </li>
-        </ul>
-        <div id="sidebar">
-            <img src="icon/iconfont-liebiao%20(2).svg" height="30px">
-        </div>
-        <div id="sidebar-detail" >
-            <ul>
-                <li><a href="index.php">首页</a></li>
-                <li><a href="article-list.php"> 文章列表</a></li>
-                <li><a href="article-category.php"> 文章分类</a></li>
-                <li><a href="#footer">关于我</a></li>
-            </ul>
-        </div>
-        <img src="icon/shadow-separator-wide-bottom.png" width="100%" height="5px">
-    </div>
-</div>
+<?include 'parts/header.php'?>
 <div id="container">
     <div>
         <div>
